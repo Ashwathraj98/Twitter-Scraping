83d7d757a78ff8b1b7354ca485238ef417bc3fbb
Ashwath Raj <ashwathrajraman98@gmail.com>
2022-10-26 14:04:16 +0530

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -32,11 +32,23 @@ if (st.sidebar.button('Submit')):
         tweets_df = pd.DataFrame(tweets_list,columns=['Datetime', 'Tweet Id', 'Text', 'Username', 'Reply', 'Retweet', 'Lang','Source', 'Likes'])
 
         st.write(tweets_df)
-        col=st.columns([1])
-        with col:
+                def convert_df(tweets_df):
+            return tweets_df.to_csv(index=False).encode('utf-8')
+        csv = convert_df(tweets_df)
+        tweets_df.to_csv('text-query-tweets.csv', sep=',', index=False)
+        json_string = tweets_df.to_json(orient='index')
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
             if st.button('Upload To Database'):
                 client = MongoClient("mongodb://localhost:27017/")
                 db = client["TW_scrap"]
                 tweets_df = db["keywords"]
                 data_dict = tweets_df.to_dict("records")
                 tweets_df.insert_many(data_dict)
+        with col2:
+            st.download_button("Download JSON File", json_string, "tweets.json", "application/json", key='download-json')
+
+        with col3:
+            st.download_button("Download CSV File", csv, "tweets.csv", "text/csv", key='download-csv')
+    else:
+        st.error('Error')
